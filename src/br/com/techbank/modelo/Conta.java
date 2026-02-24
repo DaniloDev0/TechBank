c465cfc3ac2681c5266c067e1dea5008a5e594d6
Danilo <danteste0@gmail.com>
2026-02-23 23:35:29 -0300

    refactor: blindagem dos metodos e aplicacao de encapsulamento rigoroso

--- src/br/com/techbank/modelo/Conta.java
+++ src/br/com/techbank/modelo/Conta.java
@@ -1,4 +1,65 @@
 package br.com.techbank.modelo;
 
 public class Conta {
+
+    private int agencia;
+    private int numero;
+    private String titular;
+    private double saldo;
+
+    public Conta(int agencia, int numero, String titular) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.titular = titular;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void imprimirConta() {
+        System.out.println("Agencia: " + this.agencia);
+        System.out.println("Numero: " + this.numero);
+        System.out.println("Titular: " + this.titular);
+        System.out.println("Saldo: " + this.saldo);
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+        }else  {
+            System.out.println("Valor invalido");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && this.saldo >= valor) {
+            this.saldo -= valor;
+        }else   {
+            System.out.println("Valor invalido");
+        }
+
+    }
+
+    public void transferir(double valor, Conta destino) {
+        if (valor > 0 && this.saldo >= valor) {
+            this.sacar(valor);
+            destino.depositar(valor);
+        } else {
+            System.out.println("Erro na transferência: Saldo insuficiente.");
+        }
+    }
 }
--- src/br/com/techbank/principal/Main.java
+++ src/br/com/techbank/principal/Main.java
@@ -1,7 +1,19 @@
 package br.com.techbank.principal;
 
+import br.com.techbank.modelo.Conta;
+
 public class Main {
     public static void main(String[] args) {
+        Conta conta = new Conta(1111, 2222,"Danilo");
+        Conta conta2 = new Conta(1111, 3333,"Maria");
+
+       // conta.depositar(-500);
+        //conta.transferir(200,conta2);
+        //conta.imprimirConta();
+        conta.sacar(10000);
+        conta.imprimirConta();
+       // conta2.imprimirConta();
+
 
     }
 }
